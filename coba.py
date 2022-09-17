de355a334a46ce6b082e1f504588bffdd262bc48
Qinthara Sadida Azka <113296753+qintharasa9@users.noreply.github.com>
2022-09-17 20:53:58 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,8 @@
+name = input ("?")
+match name:
+    case "hello" "hello,newman":
+        print("$0")
+    case "how you doing?":
+        print("$20")
+    case "what's happening?":
+        print("$100")
